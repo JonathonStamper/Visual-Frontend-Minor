ebeb4e31191ff3fb19fffc488b3c3e82f1cd0de6
Joey Quandt <57623466+JoeyQuandt@users.noreply.github.com>
2024-06-03 10:22:09 +0200

    feat: setup spline

---
+++ components/Earth.jsx
@@ -0,0 +1,7 @@
+import Spline from "@splinetool/react-spline";
+
+export default function Earth() {
+  return (
+    <Spline scene="https://prod.spline.design/l27iJmuUTNajGE3H/scene.splinecode" />
+  );
+}
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import { Inter } from "next/font/google";
+import Earth from "@/components/Earth";
 // import {Example} from '/Example_Image.jpg'
 
 const inter = Inter({ subsets: ["latin"] });
@@ -9,8 +10,15 @@ export default function Home() {
     <main
       className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
     >
-     <h1 className="text-8xl font-bold">Hello world</h1>
-     <Image alt='Example images' src='/Example_Image.jpg' className="w-screen h-screen absolute -z-10" width={300} height={300}/>
+      <h1 className="text-8xl font-bold">Hello world</h1>
+      <Image
+        alt="Example images"
+        src="/Example_Image.jpg"
+        className="w-screen h-screen absolute -z-10"
+        width={300}
+        height={300}
+      />
+      <Earth />
     </main>
   );
 }
