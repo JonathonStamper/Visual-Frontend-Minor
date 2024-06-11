ce8f39bf5401de7a431d0b20ed47e6048c720dbc
Joey Quandt <57623466+JoeyQuandt@users.noreply.github.com>
2024-06-11 11:06:06 +0200

    feat code

--- components/layout.jsx
+++ components/layout.jsx
@@ -3,16 +3,17 @@ import { Plus, Minus } from "lucide-react";
 import { useState } from "react";
 
 export default function Layout({ children, data }) {
-  console.log(data.current);
-  const [scale, setScale] = useState(1);
-
   const handleZoomIn = () => {
-    data.current.position.z -= 3000;
+    if (data) {
+      data.current.position.z -= 3000;
+    }
+  };
+  const handleZoomOut = () => {
+    if (data) {
+      data.current.position.z += 3000;
+    }
   };
 
-  const handleZoomOut = () => {
-    data.current.position.z += 3000;
-  };
   return (
     <>
       <main className="relative">
@@ -21,18 +22,22 @@ export default function Layout({ children, data }) {
             Portfolio.
           </Link>
           <div className="flex flex-col gap-3">
-            <button
-              className="bg-white text-black p-3 text-xl rounded-[50%]"
-              onClick={handleZoomIn}
-            >
-              <Plus />
-            </button>
-            <button
-              className="bg-white text-black p-3 text-xl rounded-[50%]"
-              onClick={handleZoomOut}
-            >
-              <Minus />
-            </button>
+            {data && (
+              <>
+                <button
+                  className="bg-white text-black p-3 text-xl rounded-[50%]"
+                  onClick={handleZoomIn}
+                >
+                  <Plus />
+                </button>
+                <button
+                  className="bg-white text-black p-3 text-xl rounded-[50%]"
+                  onClick={handleZoomOut}
+                >
+                  <Minus />
+                </button>
+              </>
+            )}
           </div>
         </div>
         {children}
--- pages/lobby.jsx
+++ pages/lobby.jsx
@@ -1,10 +1,10 @@
 import { Inter } from "next/font/google";
-import { useRef } from "react";
+import { useRef, useState } from "react";
 import Spline from "@splinetool/react-spline";
 import Layout from "@/components/layout";
-const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
+  const [showModal, setShowModal] = useState(true);
   const door = useRef();
 
   function onMouseDown(e) {
@@ -28,6 +28,25 @@ export default function Home() {
         onMouseDown={onMouseDown}
         style={{ width: "100%", height: "100vh" }}
       />
+      {showModal && (
+        <div className="absolute bg-white p-10 rounded-sm left-[50%] translate-x-[-50%] top-[50%] max-w-[425px]">
+          <h2 className="mb-5">Introduction</h2>
+          <p className="mb-5">
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sit, sint
+            ullam. Ipsam explicabo illo a ratione ipsa soluta non perferendis!
+          </p>
+          <div className="flex justify-end">
+            <button
+              className="bg-theme-Purple text-white font-medium p-3 rounded-sm"
+              onClick={() => setShowModal(false)}
+            >
+              Continue Exploring
+            </button>
+          </div>
+        </div>
+      )}
     </Layout>
   );
 }
+
+// absolute; left: 50%; transform: translateX(-50%)
