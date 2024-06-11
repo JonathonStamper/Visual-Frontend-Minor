44ad1c3860b9134024cee4a54b7dc3f3f0e732e0
JonathonStamper <96175364+JonathonStamper@users.noreply.github.com>
2024-06-11 03:33:46 +0200

    Animations

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,21 @@ import { Inter } from "next/font/google";
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
-  const loaderList = [1,2,3,4]
+  const loaderList = [
+    {color: '#C6A664',
+     number: 1
+    },
+    {color: '#909090',
+    number: 2
+   },
+   {color: '#20603D',
+   number: 3
+  },
+  {color: '#49678D',
+  number: 4
+ },
+
+  ]
 
   return (
     <main
@@ -13,14 +27,71 @@ export default function Home() {
     >
      <h1 className="animate animation--infite animation--slow slideInLeft text-8xl font-bold">Hello world</h1>
      <h1 className="animate animation--infite Rotate animation--slow">Roation</h1>
+
     <div className=" flex gap-5 items-center">
       Loading Animation 1
       <div className="loader flex gap-5">
         {loaderList.map((item, index) => (        
-        <span key={item} style={{'--i': index + 1}}></span>
+        <span key={item.number} style={{'--i': item.number, '--c': item.color}}></span>
 ))}
         
       </div>
+    </div>
+
+    <div className=" flex gap-5 justify-center items-center relative">
+      Loading Animation 2
+      <div className="loader2 flex gap-5 ite ">
+      {loaderList.map((item, index) => (        
+        <span key={item.number} style={{'--i2': item.number, '--c2': item.color}}></span>
+))}
+        
+      </div>
+    </div>
+
+    <div className=" flex gap-5 justify-center items-center relative">
+      Loading Animation 3
+      <div className="loader3 flex gap-5 ite ">
+      {loaderList.map((item, index) => (        
+        <span key={item.number} style={{'--i3': item.number, '--c3': item.color}}></span>
+))}
+        
+      </div>
+    </div>
+
+    <div className=" flex gap-5 justify-center items-center relative">
+      Loading Animation 4
+      <div className="loader4 flex gap-2 ite ">
+      {loaderList.map((item, index) => (        
+        <span key={item.number}  style={{'--i4': item.number, '--c4': item.color}}></span>
+))}
+        
+      </div>
+    </div>
+
+    <div className=" flex gap-5 justify-center items-center relative">
+      Loading Animation 5
+      <div className="loader5 flex gap-2 ite ">
+      {loaderList.map((item, index) => (        
+        <span key={item.number} className={'planet' + item.number} style={{'--i5': item.number, '--c5': item.color}}></span>
+))}
+        
+      </div>
+    </div>
+
+
+    <div className=" flex gap-5 justify-center items-center relative">
+      Loading Animation 6
+      <div className="loader6 flex gap-2 ite ">
+      {loaderList.map((item, index) => (        
+        <span key={item.number} className={'planet' + item.number} style={{'--i6': item.number, '--c6': item.color}}></span>
+))}
+        
+      </div>
+    </div>
+
+
+    <div className="glassMorphism">
+      hello worlds
     </div>
 
     
