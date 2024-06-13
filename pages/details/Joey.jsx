7f4de5411f80f7f2396b41b59ca91ca2496afb5e
Joey Quandt <57623466+JoeyQuandt@users.noreply.github.com>
2024-06-13 13:30:51 +0200

    feat(content)

--- pages/details/Joey.jsx
+++ pages/details/Joey.jsx
@@ -16,6 +16,8 @@ import Loading from "@/components/Loading";
 
 export default function Joey() {
   const [open, setIsOpen] = useState(false);
+  const [open2, setIsOpen2] = useState(false);
+  const [open3, setIsOpen3] = useState(false);
 
   const [isLoading, setIsLoading] = useState(true);
   const joeyWorld = useRef();
@@ -39,7 +41,11 @@ export default function Joey() {
     }
 
     if (e.target.name === "Leerdoel2") {
-      setIsOpen(true);
+      setIsOpen2(true);
+    }
+
+    if (e.target.name === "Leerdoel3") {
+      setIsOpen3(true);
     }
   }
 
@@ -56,6 +62,45 @@ export default function Joey() {
         <DrawerContent className="max-w-[750px] ml-auto p-5">
           <DrawerTitle>Leerdoel 1</DrawerTitle>
           <DrawerDescription>
+            Nunc ornare, tellus a iaculis condimentum, libero quam accumsan
+            massa, non egestas ligula metus eu magna. Interdum et malesuada
+            fames ac ante ipsum primis in faucibus. Fusce et metus suscipit est
+            bibendum interdum eu ut nulla. In tincidunt porttitor imperdiet.
+            Phasellus felis velit, laoreet at malesuada ac, finibus non lacus.
+            Nunc ornare, tellus a iaculis condimentum, libero quam accumsan
+            massa, non egestas ligula metus eu magna. Interdum et malesuada
+            fames ac ante ipsum primis in faucibus. Fusce et metus suscipit est
+            bibendum interdum eu ut nulla. In tincidunt porttitor imperdiet.
+            Phasellus felis velit, laoreet at malesuada ac, finibus non lacus.
+          </DrawerDescription>
+        </DrawerContent>
+      </Drawer>
+      <Drawer open={open2} onOpenChange={setIsOpen2}>
+        <DrawerContent className="max-w-[750px] ml-auto p-5">
+          <DrawerTitle>Leerdoel 2</DrawerTitle>
+          <DrawerDescription>
+            Nunc ornare, tellus a iaculis condimentum, libero quam accumsan
+            massa, non egestas ligula metus eu magna. Interdum et malesuada
+            fames ac ante ipsum primis in faucibus. Fusce et metus suscipit est
+            bibendum interdum eu ut nulla. In tincidunt porttitor imperdiet.
+            Phasellus felis velit, laoreet at malesuada ac, finibus non lacus.
+            Nunc ornare, tellus a iaculis condimentum, libero quam accumsan
+            massa, non egestas ligula metus eu magna. Interdum et malesuada
+            fames ac ante ipsum primis in faucibus. Fusce et metus suscipit est
+            bibendum interdum eu ut nulla. In tincidunt porttitor imperdiet.
+            Phasellus felis velit, laoreet at malesuada ac, finibus non lacus.
+          </DrawerDescription>
+        </DrawerContent>
+      </Drawer>
+      <Drawer open={open3} onOpenChange={setIsOpen3}>
+        <DrawerContent className="max-w-[750px] ml-auto p-5">
+          <DrawerTitle>Leerdoel 3</DrawerTitle>
+          <DrawerDescription>
+            Nunc ornare, tellus a iaculis condimentum, libero quam accumsan
+            massa, non egestas ligula metus eu magna. Interdum et malesuada
+            fames ac ante ipsum primis in faucibus. Fusce et metus suscipit est
+            bibendum interdum eu ut nulla. In tincidunt porttitor imperdiet.
+            Phasellus felis velit, laoreet at malesuada ac, finibus non lacus.
             Nunc ornare, tellus a iaculis condimentum, libero quam accumsan
             massa, non egestas ligula metus eu magna. Interdum et malesuada
             fames ac ante ipsum primis in faucibus. Fusce et metus suscipit est
--- pages/details/JoeyHouse.jsx
+++ pages/details/JoeyHouse.jsx
@@ -2,19 +2,55 @@ import Spline from "@splinetool/react-spline";
 import Loading from "@/components/Loading";
 import { useEffect, useState } from "react";
 import Layout from "@/components/layout";
+import {
+  Drawer,
+  DrawerContent,
+  DrawerDescription,
+  DrawerTitle,
+} from "@/components/ui/drawer";
 
 export default function JoeyHouse() {
   const [isLoading, setIsLoading] = useState(true);
+  const [open, setIsOpen] = useState(false);
   useEffect(() => {
     setTimeout(() => {
       setIsLoading(false);
     }, 3500);
   }, []);
+
+  function onMouseDown(e) {
+    console.log(e);
+    if (e.target.name === "Leerdoel1") {
+      setIsOpen(true);
+    }
+  }
+
   return (
     <>
       <Layout>
         {isLoading && <Loading />}
-        <Spline scene="https://prod.spline.design/eoDfixn1U745RWEh/scene.splinecode" />
+        <Spline
+          scene="https://prod.spline.design/TPlYELNnByEULOXJ/scene.splinecode"
+          onMouseDown={onMouseDown}
+        />
+        <Drawer open={open} onOpenChange={setIsOpen}>
+          <DrawerContent className="max-w-[750px] ml-auto p-5 h-full">
+            <DrawerTitle>Leerdoel 1</DrawerTitle>
+            <DrawerDescription>
+              Nunc ornare, tellus a iaculis condimentum, libero quam accumsan
+              massa, non egestas ligula metus eu magna. Interdum et malesuada
+              fames ac ante ipsum primis in faucibus. Fusce et metus suscipit
+              est bibendum interdum eu ut nulla. In tincidunt porttitor
+              imperdiet. Phasellus felis velit, laoreet at malesuada ac, finibus
+              non lacus. Nunc ornare, tellus a iaculis condimentum, libero quam
+              accumsan massa, non egestas ligula metus eu magna. Interdum et
+              malesuada fames ac ante ipsum primis in faucibus. Fusce et metus
+              suscipit est bibendum interdum eu ut nulla. In tincidunt porttitor
+              imperdiet. Phasellus felis velit, laoreet at malesuada ac, finibus
+              non lacus.
+            </DrawerDescription>
+          </DrawerContent>
+        </Drawer>
       </Layout>
     </>
   );
